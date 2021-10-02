e60a90898dbbf436d3f2db412204c27f35aba988
Anisberto <anisbertoos@gmail.com>
2021-10-02 18:18:44 -0300

    Ajuste de pilha navegação

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React from "react";
 import HomeListPokemon from "./src/pages/pokemons/listPokemons";
 
 const Stack = createNativeStackNavigator();
@@ -9,7 +9,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Search Pokemons" component={HomeListPokemon} />
+        <Stack.Screen
+          name="Pokemons"
+          component={HomeListPokemon}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
