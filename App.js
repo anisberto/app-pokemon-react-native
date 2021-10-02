0a66c248edb87c246a23cae4f876034df5e4873d
Anisberto <anisbertoos@gmail.com>
2021-10-02 03:03:11 -0300

    feat: chamada da tela de listagem pela pilha de telas

--- App.js
+++ App.js
@@ -1,29 +1,16 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-// import { createNativeStackNavigator } from "@react-navigation/native-stack";
-// import HomeListPokemon from "./src/pages/pokemons/listPokemons";
+import HomeListPokemon from "./src/pages/pokemons/listPokemons";
 
-// const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Search your favorite Pokemon</Text>
-      {/* <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="home" component={HomeListPokemon} />
-        </Stack.Navigator>
-      </NavigationContainer> */}
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Search Pokemons" component={HomeListPokemon} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    paddingTop: 20,
-    fontSize: 100,
-  },
-});
